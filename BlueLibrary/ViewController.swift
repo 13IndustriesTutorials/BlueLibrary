15973b1b2013672ca81949ee1eacff37c0adcf46
dkw5877 <dkw5877@gmail.com>
2014-07-16 18:13:16 -0700

    add delegate methods to view controller and implement data source

--- BlueLibrary/ViewController.swift
+++ BlueLibrary/ViewController.swift
@@ -8,33 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    var allAlbums:AnyObject[]?
+    var currentAlbumData:NSDictionary?
+    var currentAlbumIndex:Int
+    @IBOutlet var tableView : UITableView
+    
+    
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        
+        self.currentAlbumIndex = 0;
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        // Custom initialization
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.view.backgroundColor = UIColor(red: 0.76, green: 0.81, blue: 0.87, alpha: 1.0)
+        self.allAlbums = LibraryAPI.sharedInstance.getAlbums()
+        self.showDataForAlbumAtIndex(self.currentAlbumIndex)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView!,numberOfRowsInSection section: Int) -> Int
+    {
+        return (self.currentAlbumData!.objectForKey("titles") as AnyObject[]).count
     }
-    */
-
+    
+    func tableView(tableView: UITableView!,cellForRowAtIndexPath indexPath: NSIndexPath!) ->UITableViewCell!
+    {
+        let cellIdentifier = "cellIdentifier"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? UITableViewCell
+        
+        if !cell{
+            
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        var titles:NSArray = self.currentAlbumData!.objectForKey("titles") as AnyObject[]!
+        cell!.textLabel.text = titles.objectAtIndex(indexPath.row) as String
+        
+        var values:NSArray = self.currentAlbumData!.objectForKey("values") as AnyObject[]!
+        //cell!.detailTextLabel.text = values.objectAtIndex(indexPath.row) as String
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!)
+    {
+        
+    }
+    
+    func showDataForAlbumAtIndex(albumIndex:Int)->Void
+    {
+        if albumIndex < self.allAlbums!.count{
+            var album = self.allAlbums![albumIndex] as Album
+            self.currentAlbumData = album.tableRepresentation()
+        }
+        else{
+            self.currentAlbumData = nil;
+        }
+        self.tableView.reloadData()
+    }
 }
